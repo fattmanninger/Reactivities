8b1840df5ef31db078fccbff0f1549e0becc81e8
Fattinger <thomas.fattinger@oeticket.com>
2021-09-24 21:25:52 +0200

    End of Section 2

---
+++ Domain/Activity.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Domain
+{
+    public class Activity
+    {
+        // primary key
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        public string City { get; set; }
+
+        public string Venue { get; set; }
+    }
+}
